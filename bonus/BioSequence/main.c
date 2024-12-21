217e979efa362aaab5aa55f9f62b00fb180c53f8
rsegerie <rsegerie@cern.ch>
2024-12-21 10:56:28 +0100

    update biosequence

--- bonus/BioSequence/main.c
+++ bonus/BioSequence/main.c
@@ -2,74 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <omp.h> 
-#include "../../Random/Random/MTwistEngine.h" 
+#include <omp.h>
+#include "../../Random/Random/MTwistEngine.h"
 
 #define BASES "ACGT"
-#define TARGET_SEQUENCE "AAATTTGCGTTCGATTAG" // "GATTACA"
-#define SEQUENCE_LENGTH 18 // 7 
-#define NUM_REPLICATIONS 1
-#define CONFIDENCE_LEVEL 1.96 
+#define TARGET_SEQUENCE "AAATTTGCGTTCGATTAG"
+#define SEQUENCE_LENGTH 18
+#define NUM_REPLICATIONS 40
+#define CONFIDENCE_LEVEL 1.96
 
-char generate_random_base(CLHEP::MTwistEngine *mt_engine) {
-    int index = (int)(mt_engine->flat() * 4); 
+char base(CLHEP::MTwistEngine *mt_engine) {
+    int index = (int)(mt_engine->flat() * 4);
     return BASES[index];
 }
 
-void generate_random_sequence(CLHEP::MTwistEngine *mt_engine, char *sequence, int length) {
+void sequence(CLHEP::MTwistEngine *mt_engine, char *sequence, int length) {
     for (int i = 0; i < length; i++) {
-        sequence[i] = generate_random_base(mt_engine);
+        sequence[i] = base(mt_engine);
     }
-    sequence[length] = '\0'; 
+    sequence[length] = '\0';
 }
 
-int find_target_sequence(CLHEP::MTwistEngine *mt_engine, const char *target_sequence) {
+int find_sequence(CLHEP::MTwistEngine *mt_engine, const char *target_sequence, int sequence_length) {
     int attempts = 0;
-    char generated_sequence[SEQUENCE_LENGTH + 1];
+    char generated_sequence[sequence_length + 1];
     do {
-        generate_random_sequence(mt_engine, generated_sequence, SEQUENCE_LENGTH);
+        sequence(mt_engine, generated_sequence, sequence_length);
         attempts++;
     } while (strcmp(generated_sequence, target_sequence) != 0);
     return attempts;
 }
 
+void calculate(CLHEP::MTwistEngine *mt_engine, const char *target_sequence) {
+    int full_length = strlen(target_sequence);
+    for (int length = 1; length <= full_length; length++) {
+        char sub_target[length + 1];
+        strncpy(sub_target, target_sequence, length);
+        sub_target[length] = '\0';
 
-void run_simulation(int num_replications) {
-    int attempts[NUM_REPLICATIONS];
-    double mean = 0.0, std_dev = 0.0, confidence_interval_lower, confidence_interval_upper;
+        int attempts = find_sequence(mt_engine, sub_target, length);
+        double probability = pow(0.25, length);
 
-    #pragma omp parallel for
-    for (int i = 0; i < num_replications; i++) {
-        CLHEP::MTwistEngine mt_engine(i + 42); 
-        attempts[i] = find_target_sequence(&mt_engine, TARGET_SEQUENCE);
-        printf("Replication %d\t: Attempts  = %d\n", i + 1, attempts[i]);
+        printf("sub-sequence: %s\n", sub_target);
+        printf("* attempts: %d\n", attempts);
+        printf("* probability (theorical): %.10f\n", probability);
+        printf("===============================\n");
     }
-
-    for (int i = 0; i < num_replications; i++) {
-        mean += attempts[i];
-    }
-    mean /= num_replications;
-
-
-    for (int i = 0; i < num_replications; i++) {
-        std_dev += pow(attempts[i] - mean, 2);
-    }
-    std_dev = sqrt(std_dev / num_replications);
-
-    confidence_interval_lower = mean - (CONFIDENCE_LEVEL * std_dev / sqrt(num_replications));
-    confidence_interval_upper = mean + (CONFIDENCE_LEVEL * std_dev / sqrt(num_replications));
-
-    printf("\n=== Results ===\n");
-    printf("Number of replications\t: %d\n", num_replications);
-    printf("Target sequence\t %s\n", TARGET_SEQUENCE);
-    printf("Sequence length\t: %d\n", SEQUENCE_LENGTH);
-    printf("Mean number of attempts\t: %.2f\n", mean);
-    printf("Standard deviation of attempts\t %.2f\n", std_dev);
-    printf("95%% Confidence interval for attempts: [%.2f, %.2f]\n", confidence_interval_lower, confidence_interval_upper);
 }
 
 int main() {
-    printf("Starting the simulation to find the target sequence : %s\n", TARGET_SEQUENCE);
-    run_simulation(NUM_REPLICATIONS);
+    printf("\n** Target sequence: %s\n", TARGET_SEQUENCE);
+
+    // Initialize random engine and analyze sub-sequences
+    CLHEP::MTwistEngine mt_engine(42);
+    calculate(&mt_engine, TARGET_SEQUENCE);
+
     return 0;
 }
